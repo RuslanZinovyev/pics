3001029c76ad912da786c687d58c20ea17241c19
rzinovyev <ruslan.zinovyev@hotmail.com>
2022-01-09 21:56:56 -0500

    making api request with react and thir party library unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
 import React from "react";
+import Unsplash from "../api/Unsplash";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  state = {
+    images: [],
+  };
+
+  onSearchSubmit = async (term) => {
+    const response = await Unsplash.get("/search/photos", {
+      params: { query: term },
+    });
+
+    this.setState({
+      images: response.data.results,
+    });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
