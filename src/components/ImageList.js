f43e28e8df45e995b63113e50b5d96e8def5294f
rzinovyev <ruslan.zinovyev@hotmail.com>
2022-01-11 21:28:51 -0500

    implemented image search app

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,1 +1,13 @@
 import React from "react";
+
+const ImageList = (props) => {
+  console.log(props.images[0]);
+
+  const images = props.images.map(({ description, id, urls }) => {
+    return <img alt={description} key={id} src={urls.regular} width="1130px" />;
+  });
+
+  return <div>{images}</div>;
+};
+
+export default ImageList;
