ac3dc4859305730491e2ea1543085d8907f23b72
rzinovyev <ruslan.zinovyev@hotmail.com>
2022-01-13 22:49:22 -0500

    refactoring

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 const ImageList = (props) => {
-  console.log(props.images[0]);
-
   const images = props.images.map(({ description, id, urls }) => {
     return <img alt={description} key={id} src={urls.regular} width="1130px" />;
   });
